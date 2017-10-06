7e0d2d39f51ce91c7c398a46f6b7945bfa4fe5c5
GrapeGun <1160874344@qq.com>
2017-10-06 17:26:12 +0800

    编辑地址, 删除地址

--- js/address.js
+++ js/address.js
@@ -4,7 +4,11 @@ new Vue({
         addressList: [],
         limitNum: 3,
         currentIndex: 0,
-        shippingMethod: 1
+        shippingMethod: 1,
+        currentAddress: {userName: "", tel: "", streetName: ""},
+        currentIndex: 0,
+        editFlag: false,
+        delFlag: false
     },
     mounted: function () {
         this.$nextTick(function () {
@@ -38,18 +42,43 @@ new Vue({
             
         },
         clearDefault: function () {
-            this.addressList.forEach(function (item, index) {
-                if (item.isDefault) {
-                    item.isDefault = false;
+            this.addressList.forEach(function (address, index) {
+                if (address.isDefault) {
+                    address.isDefault = false;
                 }
             });
+        },
+        unblock: function () {
+            this.editFlag = false;
+            this.delFlag = false;
+        },
+        editAddress: function (address, index) {
+            // 修改地址页面, 编辑的数据不直接在地址列表页面同步显示.
+            this.editFlag = true;
+            this.currentAddress.userName = address.userName;
+            this.currentAddress.streetName = address.streetName;
+            this.currentAddress.tel = address.tel;
+            this.currentIndex = index;
+        },
+        saveAddress: function () {
+            let address = this.addressList[this.currentIndex];
+            address.userName = this.currentAddress.userName;
+            address.streetName = this.currentAddress.streetName;
+            address.tel = this.currentAddress.tel;
+            this.editFlag = false;
+        },
+        delConfirm: function(index) {
+            this.currentIndex = index;
+            this.delFlag = true;
+        },
+        delAddress: function () {
+            this.addressList.splice(this.currentIndex, 1);
+            this.delFlag = false;
+
+        },
+        addNewAddress: function() {
+            alert("前端能够处理姓名, 街道名称, 电话, 是否默认, 其他字段通过后台生成更为合适, 此功能暂不实现了")
         }
     }
-
-
-
-
-
-
 
 });
